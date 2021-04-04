f926cf9f54ef6a1539907305e9d44d171b0b1037
Xinyi Wu <71575624+Xinyi-Wu@users.noreply.github.com>
2021-04-04 10:59:52 +0800

    Update pca_vs_svd.m

--- PCA/pca_vs_svd.m
+++ PCA/pca_vs_svd.m
@@ -4,11 +4,14 @@ x = x(:,2:end);
 tau = 1;
 
 % iterate experiment
+Ab = 3;
+Re = 3;
+delta = 0;
 n = 30;
 iter = 1:1:n;
-err = zeros(2,n);
+err = [];
 for i=1:n
-    err(:,i) = mysvd_pca(x,tau,0);
+    err(:,i) = mysvd_pca(x, tau, Ab, Re, delta);
 end
 
 plot(iter,err(1,:),'b-',iter,err(2,:),'k-','Linewidth',2);
