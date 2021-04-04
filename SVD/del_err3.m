c5811ae7105c654a7817018465858c913b0507ab
Xinyi Wu <71575624+Xinyi-Wu@users.noreply.github.com>
2021-04-04 10:52:47 +0800

    Update del_err3.m

--- SVD/del_err3.m
+++ SVD/del_err3.m
@@ -10,15 +10,15 @@ Indi = 2;
 n_del = size(x,2)-2;  
 del_k = 0:1:n_del;
 n_iter = 60;
-err = zeros(n_iter, n_del);
+err_all = [];
 
 % iterate experiment
 for i=1:n_iter
     for j=1:(n_del+1)
-        [a,b,err(i,j)] = mypca3(x, tau, Ab, Di, Indi, del_k(j));
+        [ER_svd, err, err_all(i,j)] = mysvd3(x, tau, Ab, Di, Indi, del_k(j));
     end
 end
 
-plot(del_k,mean(err),'b-','Linewidth',2);
+plot(del_k,mean(err_all),'b-','Linewidth',2);
 xlabel('Number of Reduation on Rank: \Delta k'); ylabel('Error');
 grid on
